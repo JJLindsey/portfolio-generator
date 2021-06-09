22156e2d87e44ae93b10f00c7aefa3e88f16757f
JJLindsey <jlindsey010@gmail.com>
2021-06-09 12:56:40 -0500

    validate fnction for About section

--- app.js
+++ app.js
@@ -70,7 +70,14 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }   else {
+                    return false;
+                }
+            }
         }
     ]);
 };
